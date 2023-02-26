e004e6724cbf9687833131cb40e623bd99de5656
chidambararaja.b <chidambararaja.b@accubits.com>
2023-02-27 00:24:26 +0530

    [CONTACT]

--- components/UI/Contact.jsx
+++ components/UI/Contact.jsx
@@ -14,9 +14,17 @@ const Contact = () => {
             <SectionSubtitle subtitle="Contact me" />
             <h3 className="mt-4 mb-4">Contact with me</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-              beatae ratione iusto cum. Doloremque suscipit eius provident error
-              labore est reprehenderit, exercitationem corrupti earum deleniti!
+              Hey there! I'm really excited about the opportunity to join your
+              company and be a part of your team. I'm confident that I've got
+              the skills and experience you're looking for, and I'm committed to
+              making a positive impact from day one. Let's start this journey
+              together and see where it takes us!
+              {/* Hey there! I'm here to lend a hand with your next application
+              development project. I've got the skills and experience to help
+              you get the job done right, and I'm committed to delivering
+              results that you can trust. Let's work together to make your next
+              project a success! Looking forward to start the journy on your
+              company. */}
             </p>
 
             <ul className={`${classes.contact__info__list}`}>
@@ -24,38 +32,50 @@ const Contact = () => {
                 <span>
                   <i className="ri-map-pin-line"></i>
                 </span>
-                <p>Sylhet - Bangladesh</p>
+                <p>Looking for hybride (any location) or remote work</p>
+              </li>
+              <li className={`${classes.info__item}`}>
+                <span>
+                  <i className="ri-map-pin-line"></i>
+                </span>
+                <p>Current Location - Chennai, India</p>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-mail-line"></i>
                 </span>
-                <p>muhib5532@gmail.com</p>
+                <p>chidambararaja97@gmail.com</p>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-phone-line"></i>
                 </span>
-                <p>+088123456789</p>
+                <p>+91 8682992677</p>
               </li>
             </ul>
 
             <div className={`${classes.social__links}`}>
-              <Link href="#">
-                <i className="ri-youtube-line"></i>
-              </Link>
-              <Link href="#">
+              <a
+                target="_blank"
+                href="https://github.com/chidambararajab"
+                rel="noopener noreferrer"
+              >
                 <i className="ri-github-line"></i>
-              </Link>
-              <Link href="#">
-                <i className="ri-facebook-line"></i>
-              </Link>
-              <Link href="#">
+              </a>
+              <a
+                target="_blank"
+                href="https://www.linkedin.com/in/chidambararajab/"
+                rel="noopener noreferrer"
+              >
                 <i className="ri-linkedin-line"></i>
-              </Link>
-              <Link href="#">
+              </a>
+              <a
+                target="_blank"
+                href="https://www.instagram.com/thechidshow/"
+                rel="noopener noreferrer"
+              >
                 <i className="ri-instagram-line"></i>
-              </Link>
+              </a>
             </div>
           </Col>
 
